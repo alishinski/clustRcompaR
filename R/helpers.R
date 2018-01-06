aa7a41a90018a26d58ea15461c84ae18f3a87c3a
Joshua Rosenberg <jrosen@msu.edu>
2018-01-06 10:16:08 -0500

    add data

---
+++ R/helpers.R
@@ -0,0 +1,13 @@
+#' data of the inaugural addresses by every United States President (from the quanteda package)
+#'
+#' @source http://docs.quanteda.io/
+#' @format Data frame with columns
+#' #' \describe{
+#'   \item{texts}{text contents of the inaugural addresses}
+#'   \item{Year}{year of the address}
+#'   \item{President}{last name of the President}
+#'   \item{FirstName}{first name of the President}
+#' }
+#' @import tibble
+
+"inaugural_addresses"
---
+++ data-raw/proc_raw_data.R
@@ -0,0 +1,3 @@
+# x <- quanteda::data_corpus_inaugural$documents
+# inaugural_addresses <- dplyr::as_tibble(x)
+# usethis::use_data(inaugural_addresses, overwrite = T)
